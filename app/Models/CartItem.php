c171103b609e9f8194d172db01de92123f8d0678
anggrainid <anggraini.d@mail.ugm.ac.id>
2022-07-21 14:23:20 +0700

    soft delete

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CartItem extends Model
 {
     //use HasFactory;
+    use SoftDeletes;
+    
     protected $fillable = [
         'invoice_id',
         'sku_code',
--- app/Models/InvoiceItem.php
+++ app/Models/InvoiceItem.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class InvoiceItem extends Model
 {
-    use HasFactory;
+    //use HasFactory;
+    use SoftDeletes;
     protected $fillable = [
         'invoice_id',
         'sku_code',
